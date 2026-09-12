ad05d4890c026fb815e2912d453d17b0b57541cb
Gabriel de Oliveira Flores <mainofftv@gmail.com>
2026-09-12 19:15:29 -0300

    Speedup building process

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "This is a simple build script for quickly setting up the project and running tests;
+for production use, it is recommended to create your own script or perform a manual build."
+
 rm -fr build
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build/ -j$(nproc)
